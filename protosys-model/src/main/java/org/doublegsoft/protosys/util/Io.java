aeda97900e2939b1d88e0c0552dd33cb7b10a9bb
Christian Gann <guo.guo.gan@gmail.com>
2026-05-03 21:09:19 +0800

    daily development

--- protosys-model/src/main/java/org/doublegsoft/protosys/util/Io.java
+++ protosys-model/src/main/java/org/doublegsoft/protosys/util/Io.java
@@ -34,7 +34,7 @@ import java.util.Map;
  */
 public class Io {
 
-  private final static Configuration FREEMARKER = new Configuration(Configuration.getVersion());
+  private final static Configuration FREEMARKER = new Configuration(Configuration.VERSION_2_3_34);
 
   private final static VelocityEngine VELOCITY = new VelocityEngine();
 
